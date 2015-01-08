e164777df77aa47b2417e1001b27ed4c5fbaf3b0
Derek Mitchell <dmitchell356@gmail.com>
2015-01-08 02:36:45 -0500

    Tiles and other stuff

---
+++ Assets/Scripts/TurnManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurnManager : Singleton<TurnManager> {
+
+	private int current_player = 0;
+
+	public static int CurrentPlayer{
+		get{
+			return Instance.current_player;
+		}
+	}
+}
