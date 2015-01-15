cacbf8d02e2c9843597ab47cf2090703760fdf4e
Derek Mitchell <dmitchell356@gmail.com>
2015-01-14 23:41:38 -0500

    Finished up stages.

---
+++ Assets/Scripts/Actions/TakeVegetable.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TakeVegetable : Action {
+	public override void Execute ()
+	{
+		Supply.AddStock(Resource.ResourceType.Vegetable,1);
+	}
+}
