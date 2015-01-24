e950e4a01017d17e96cf1d676ba72bcdedf40740
Derek Mitchell <dmitchell356@gmail.com>
2015-01-24 01:57:18 -0500

    Updates

---
+++ Assets/Scripts/Interface.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Enum = System.Enum;
+
+public class Interface : Singleton<Interface> {
+	public GameObject confirm_prefab;
+	public Transform confirm_location;
+	private Dictionary<Resource.ResourceType,int> modifier_amounts;
+
+	public void Awake(){
+		modifier_amounts = new Dictionary<Resource.ResourceType, int>();
+		resetModifiers();
+	}
+
+	static public GameObject awaitConfirmation(){
+		GameObject button = Instantiate(Instance.confirm_prefab,Instance.confirm_location.position,Instance.confirm_location.rotation) as GameObject;
+		button.transform.parent = Instance.transform;
+		return button;
+	}
+
+	static public void setModifier(Resource.ResourceType resource, int amount){
+		if(Instance.modifier_amounts.ContainsKey(resource)){
+			Instance.modifier_amounts[resource] = amount;
+		}
+	}
+
+	static public void resetModifiers(){
+		foreach(Resource.ResourceType resource in Enum.GetValues(typeof(Resource.ResourceType))){
+			Instance.modifier_amounts[resource] = 0;
+		}
+	}
+
+	static public int getModifier(Resource.ResourceType resource){
+		if(Instance.modifier_amounts.ContainsKey(resource)){
+			return Instance.modifier_amounts[resource];
+		}
+		return 0;
+	}
+}
--- Assets/Scripts/UI/UI_ResourceCounter.cs
+++ Assets/Scripts/UI/UI_ResourceCounter.cs
@@ -5,8 +5,12 @@ public class UI_ResourceCounter : MonoBehaviour {
 
 //	private Supply playerSupply;
 	public TextMesh display;
+	public TextMesh modifier;
+	public TextMesh modifier_amount;
 	public Resource.ResourceType resource;
 	public Transform token_location;
+	public Color positive_color;
+	public Color negative_color;
 
 	// Use this for initialization
 	void Start () {
@@ -25,5 +29,20 @@ public class UI_ResourceCounter : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		display.text = Supply.CheckStock(resource).ToString();
+		int m = Interface.getModifier(resource);
+		if(m > 0){
+			modifier.text = "+";
+			modifier.color = positive_color;
+			modifier_amount.text = m.ToString();
+			modifier_amount.color = positive_color;
+		}else if(m < 0){
+			modifier.text = "-";
+			modifier.color = negative_color;
+			modifier_amount.text = (-m).ToString();
+			modifier_amount.color = negative_color;
+		}else{
+			modifier.text = "";
+			modifier_amount.text = "";
+		}
 	}
 }
